3adc693d03ea54eb4efb7c93c3608dbbf9f967e6
minh.trinh 🐛 <tvminh2611@gmail.com>
2023-12-13 23:45:08 +0700

    add some funny stuffs

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,8 @@ const NavigationBar = () => {
     <div className="flex h-[72px] bg-black px-[1rem]">
       <div className="container mx-auto flex  w-full items-center justify-between">
         <img
-          src="https://sleap.io/assets/logo/sleap-logo-white.svg"
+          src="https://usagif.com/wp-content/uploads/2020/b72nv6/partyparrt-21.gif"
+          // src="https://sleap.io/assets/logo/sleap-logo-white.svg"
           alt=""
           className="h-10"
         />
